c2201afe1dd9eae7e1fd0e0f3cb04d8e7a4b7e5f
Archit <archit@bitcs.in>
2020-12-25 14:22:57 +0530

    webview working

--- app/src/main/java/com/bitcs/desitalent/changalite/MainActivity.java
+++ app/src/main/java/com/bitcs/desitalent/changalite/MainActivity.java
@@ -3,12 +3,28 @@ package com.bitcs.desitalent.changalite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.web_view);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentInWebView();
+    }
+
+    private void setContentInWebView() {
+        String html = "<html><body><h1>Hello, World!</h1></body></html>";
+        mWebView.loadData(html, "text/html", "UTF-8");
     }
 }
