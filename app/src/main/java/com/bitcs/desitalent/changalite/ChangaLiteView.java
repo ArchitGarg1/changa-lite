a30d243a61baabf80cab2d4646e9610f1883b8a5
Archit <archit@bitcs.in>
2021-01-11 16:44:32 +0530

    fixed black screen

--- app/src/main/java/com/bitcs/desitalent/changalite/ChangaLiteView.java
+++ app/src/main/java/com/bitcs/desitalent/changalite/ChangaLiteView.java
@@ -3,6 +3,7 @@ package com.bitcs.desitalent.changalite;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -62,14 +63,21 @@ public class ChangaLiteView extends ConstraintLayout {
     private void setContentInWebView() {
 //        AppId = "f0626ff3-ba6b-41fc-a259-75ea2a661b6e";
         mWebView = findViewById(R.id.web_view);
+        WebSettings settings = mWebView.getSettings();
         constraintLayout = findViewById(R.id.layout);
-        String html = "<html><body><div id='changa-slider' appid=" + AppId + "></div> <script src='https://www.changa.in/assets/js/changa-lite.js'></script></body></html>";
+        String html = "<html><body><div id='changa-slider' appid=" + AppId + " slider-type='vertical'></div> <script src='https://www.changa.in/assets/js/changa-lite.js'></script></body></html>";
         mWebView.loadData(html, "text/html", "UTF-8");
         mWebView.setWebViewClient(new MyBrowser());
-        mWebView.getSettings().setLoadsImagesAutomatically(true);
-        mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setVerticalScrollBarEnabled(false);
         mWebView.setHorizontalScrollBarEnabled(false);
+        settings.setPluginState(WebSettings.PluginState.ON);
+        settings.setLoadsImagesAutomatically(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowContentAccess(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        settings.setUseWideViewPort(false);     // to switch in desktop and mobile site
     }
 
     private static class MyBrowser extends WebViewClient {
