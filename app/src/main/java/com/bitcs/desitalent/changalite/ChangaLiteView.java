e4966d3ee378e5e9ce42a8d2f73a9172490f0af2
Archit <archit@bitcs.in>
2021-01-13 17:59:33 +0530

    updated slider => button

--- app/src/main/java/com/bitcs/desitalent/changalite/ChangaLiteView.java
+++ app/src/main/java/com/bitcs/desitalent/changalite/ChangaLiteView.java
@@ -1,11 +1,18 @@
 package com.bitcs.desitalent.changalite;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,8 +21,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 public class ChangaLiteView extends ConstraintLayout {
 
     private ConstraintLayout constraintLayout;
-    private WebView mWebView;
     private String AppId;
+    private ImageView imageView;
 
     public ChangaLiteView(@NonNull Context context) {
         super(context);
@@ -40,36 +47,58 @@ public class ChangaLiteView extends ConstraintLayout {
     private void init(Context context) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.layout_changa_lite, this);
+        constraintLayout = findViewById(R.id.layout);
 
-        setContentInWebView();
+        imageView = findViewById(R.id.main);
+        imageView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchPopup();
+            }
+        });
     }
 
     public void setAppId(String AppId) {
         this.AppId = AppId;
-        setContentInWebView();
     }
 
     @Override
     public void setBackgroundColor(int color) {
-        if (mWebView != null) {
-            mWebView.setBackgroundColor(color);
+        if (constraintLayout != null) {
+            constraintLayout.setBackgroundColor(color);
         }
+    }
+
+    public void setIcon(Drawable drawable) {
+        imageView.setImageDrawable(drawable);
+    }
+
+    public void setIcon(Bitmap bm) {
+        imageView.setImageBitmap(bm);
     }
 
 //    public void setParam() {
 //        constraintLayout.setBackgroundColor(R.attr.param1);
 //    }
 
-    private void setContentInWebView() {
-//        AppId = "f0626ff3-ba6b-41fc-a259-75ea2a661b6e";
-        mWebView = findViewById(R.id.web_view);
+    private static class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    private void launchPopup() {
+        WebView mWebView = new WebView(getContext());
         WebSettings settings = mWebView.getSettings();
-        constraintLayout = findViewById(R.id.layout);
-        String html = "<html><body><div id='changa-slider' appid=" + AppId + " slider-type='vertical'></div> <script src='https://www.changa.in/assets/js/changa-lite.js'></script></body></html>";
-        mWebView.loadData(html, "text/html", "UTF-8");
+//        String html = "<html><body><div id='changa-slider' appid=" + AppId + " slider-type='vertical'></div> <script src='https://changa-lite-app.surge.sh/main.js'></script></body></html>";
+//        mWebView.loadData(html, "text/html", "UTF-8");
+        mWebView.loadUrl("https://changa-lite-app.surge.sh/?appid="+AppId);
         mWebView.setWebViewClient(new MyBrowser());
         mWebView.setVerticalScrollBarEnabled(false);
         mWebView.setHorizontalScrollBarEnabled(false);
+        mWebView.setBackgroundColor(Color.BLACK);
         settings.setPluginState(WebSettings.PluginState.ON);
         settings.setLoadsImagesAutomatically(true);
         settings.setJavaScriptEnabled(true);
@@ -78,13 +107,11 @@ public class ChangaLiteView extends ConstraintLayout {
         settings.setLoadWithOverviewMode(true);
         settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
         settings.setUseWideViewPort(false);     // to switch in desktop and mobile site
-    }
 
-    private static class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+        RelativeLayout.LayoutParams paramsWebView = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        Dialog dialog = new Dialog(getContext(), android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        dialog.addContentView(mWebView, paramsWebView);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.show();
     }
 }
